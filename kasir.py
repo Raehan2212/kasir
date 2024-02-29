55c3a0f0af06dac7860109eea6e398ba2752010f
Raehan2212 <160814899+Raehan2212@users.noreply.github.com>
2024-02-29 11:23:58 +0700

    Add files via upload

---
+++ kasir.py
@@ -0,0 +1,11 @@
+#ucup membeli barang ke warung
+Rokok=20000
+minyak=25000
+gula=11000
+
+saldo=100000
+
+diskon=10000
+#hitung jumlah kembalian ucup
+kembalian=saldo-(Rokok+minyak+gula-diskon)
+print("kembalian Ucup=",kembalian)
